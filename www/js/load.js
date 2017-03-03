8e1e20b29a86b4dbe2d8ec6ef90c9d17a41111c1
tmoreti <thiago.moreti@gmail.com>
2017-03-03 16:03:37 -0300

    Add files via upload

--- www/js/load.js
+++ www/js/load.js
@@ -1,23 +1,23 @@
-$( document ).bind( "mobileinit", function() {
+/*$( document ).bind( "mobileinit", function() {
     $.mobile.allowCrossDomainPages = true;
-    $.mobile.touchOverflowEnabled = true;
+    $.mobile.touchOverflowEnabled = false;
 	$.mobile.loader.prototype.options.text = "Carregando..";
 	$.mobile.loader.prototype.options.textVisible = True;
 	$.mobile.loader.prototype.options.theme = "a";
 	$.mobile.loader.prototype.options.html = "";
-});
+});*/
 $.ajax({
   url: "footer.html",
-  cache: false,
-  dataType: "html"
+  dataType: "html",
+  async: false
 }).done(function(html) {
   $('footer').html(html);
 });
 
 $.ajax({
   url: "header.html",
-  cache: false,
-  dataType: "html"
+  dataType: "html",
+  async: false
 }).done(function(html) {
   $('header').html(html);
 });
