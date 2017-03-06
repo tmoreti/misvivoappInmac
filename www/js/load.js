379136b7321ad3cd03f6c08051038528938d0feb
tmoreti <thiago.moreti@gmail.com>
2017-03-06 17:55:47 -0300

    Update load.js

--- www/js/load.js
+++ www/js/load.js
@@ -1,11 +1,11 @@
-$( document ).bind( "mobileinit", function() {
+/*$( document ).bind( "mobileinit", function() {
     $.mobile.allowCrossDomainPages = true;
     $.mobile.touchOverflowEnabled = false;
 	$.mobile.loader.prototype.options.text = "Carregando..";
 	$.mobile.loader.prototype.options.textVisible = True;
 	$.mobile.loader.prototype.options.theme = "a";
 	$.mobile.loader.prototype.options.html = "";
-});
+});*/
 $.ajax({
   url: "footer.html",
   dataType: "html",
