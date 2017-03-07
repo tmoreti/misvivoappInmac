1ada728f42d25d921f89c6818625f6ce7464de73
tmoreti <thiago.moreti@gmail.com>
2017-03-07 10:40:30 -0300

    Update load.js

--- www/js/load.js
+++ www/js/load.js
@@ -6,8 +6,8 @@
 	$.mobile.loader.prototype.options.theme = "a";
 	$.mobile.loader.prototype.options.html = "";
 });*/
-$.mobile.loading('show');
-for(i=0;i<1000;i++){
+
+
   $.ajax({
     url: "footer.html",
     dataType: "html",
@@ -23,5 +23,37 @@ for(i=0;i<1000;i++){
   }).done(function(html) {
     $('header').html(html);
   });
-}
-$.mobile.loading('hide');
+
+  /*$.ajax({
+    url: 'http://www.livemusicapp.com.br/misvivoapp/itens.php',
+    dataType: "html",
+    async: false
+  }).done(function(html) {
+    $('header').html(html);
+  });*/
+
+  var email=localStorage.getItem('email');
+  $.ajax({
+      url:'http://www.livemusicapp.com.br/misvivoapp/usuarios.php',
+      method: 'POST',
+      dataType: 'html',
+      data:{
+          tipo: 'verificar',
+          email: email
+      },
+      beforeSend: function() {
+              // This callback function will trigger before data is sent
+              $.mobile.loading('show', {theme:"b"}); // This will show AJAX spinner
+          },
+  }).done(function(html){
+      $.mobile.loading('hide');
+      if(html==0){
+          $.mobile.changePage("#cadastro");
+          //$('body').fadeIn('fast');
+      }else{
+          window.plugins.OneSignal.sendTag("fl_autorizado", "1");
+          $.mobile.changePage("#logado");
+          //$('body').fadeIn('fast');
+      }
+  });
+
