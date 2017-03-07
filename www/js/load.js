c2cdc9b4c78d99a221404fade4cddf26aa5a4fdc
tmoreti <thiago.moreti@gmail.com>
2017-03-07 11:24:04 -0300

    Update load.js

--- www/js/load.js
+++ www/js/load.js
@@ -1,16 +1,3 @@
-//$( document ).bind( "mobileinit", function() {
-   /* $.mobile.allowCrossDomainPages = true;
-    $.mobile.touchOverflowEnabled = false;
-	$.mobile.loader.prototype.options.text = "Carregando..";
-	$.mobile.loader.prototype.options.textVisible = True;
-	$.mobile.loader.prototype.options.theme = "a";
-	$.mobile.loader.prototype.options.html = "";*/
-//});
-/*
-$(document).on("pagecreate",function(){
-  verificarCadastro();
-})*/
-
   $.ajax({
     url: "footer.html",
     dataType: "html",
@@ -27,13 +14,4 @@ $(document).on("pagecreate",function(){
     $('header').html(html);
   });
   
-
-  /*$.ajax({
-    url: 'http://www.livemusicapp.com.br/misvivoapp/itens.php',
-    dataType: "html",
-    async: false
-  }).done(function(html) {
-    $('header').html(html);
-  });
-
 
