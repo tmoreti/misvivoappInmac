c83d6c75841eb6a12ee20c3036f1b37caeb7439a
tmoreti <thiago.moreti@gmail.com>
2017-03-07 10:53:07 -0300

    Update load.js

--- www/js/load.js
+++ www/js/load.js
@@ -1,14 +1,11 @@
-/*$( document ).bind( "mobileinit", function() {
-    $.mobile.allowCrossDomainPages = true;
+$( document ).bind( "mobileinit", function() {
+   /* $.mobile.allowCrossDomainPages = true;
     $.mobile.touchOverflowEnabled = false;
 	$.mobile.loader.prototype.options.text = "Carregando..";
 	$.mobile.loader.prototype.options.textVisible = True;
 	$.mobile.loader.prototype.options.theme = "a";
-	$.mobile.loader.prototype.options.html = "";
-});*/
-
-
-  $.ajax({
+	$.mobile.loader.prototype.options.html = "";*/
+   $.ajax({
     url: "footer.html",
     dataType: "html",
     async: false
@@ -23,37 +20,5 @@
   }).done(function(html) {
     $('header').html(html);
   });
-
-  /*$.ajax({
-    url: 'http://www.livemusicapp.com.br/misvivoapp/itens.php',
-    dataType: "html",
-    async: false
-  }).done(function(html) {
-    $('header').html(html);
-  });*/
-
-  var email=localStorage.getItem('email');
-  $.ajax({
-      url:'http://www.livemusicapp.com.br/misvivoapp/usuarios.php',
-      method: 'POST',
-      dataType: 'html',
-      data:{
-          tipo: 'verificar',
-          email: email
-      },
-      beforeSend: function() {
-              // This callback function will trigger before data is sent
-              $.mobile.loading('show', {theme:"b"}); // This will show AJAX spinner
-          },
-  }).done(function(html){
-      $.mobile.loading('hide');
-      if(html==0){
-          $.mobile.changePage("#cadastro");
-          //$('body').fadeIn('fast');
-      }else{
-          window.plugins.OneSignal.sendTag("fl_autorizado", "1");
-          $.mobile.changePage("#logado");
-          //$('body').fadeIn('fast');
-      }
-  });
-
+  verificarCadastro();
+});
