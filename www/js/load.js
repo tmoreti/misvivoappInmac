8f135d99f28150ed1b805759532fe1b063e89679
tmoreti <thiago.moreti@gmail.com>
2017-03-07 12:00:27 -0300

    Add files via upload

--- www/js/load.js
+++ www/js/load.js
@@ -1,27 +1,22 @@
+$(document).on("mobileinit",function() {
+    $.mobile.autoInitializePage = false;
+}); 
+$.ajax({
+  url: "footer.html",
+  dataType: "html",
+  async: false
+}).done(function(html) {
+  $('footer').html(html);
+});
 
+$.ajax({
+  url: "header.html",
+  dataType: "html",
+  async: false
+}).done(function(html) {
+  $('header').html(html);
+});
 
-
-
-  
-
-$(document).on("pagecreate",function(){
-  $.ajax({
-    url: "footer.html",
-    dataType: "html",
-    async: false
-  }).done(function(html) {
-    $('footer').html(html);
-  });
-
-  $.ajax({
-    url: "header.html",
-    dataType: "html",
-    async: false
-  }).done(function(html) {
-    $('header').html(html);
-  });
-  verificarCadastro();
-})
 
  
 
